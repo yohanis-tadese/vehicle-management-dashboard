e4725c4d5b033a9df6d3d6fb1a0f3b2a2df27df2
Yohanis Tadese <127344303+yohanis-tadese@users.noreply.github.com>
2024-12-13 23:19:52 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "https://vehicle-management-system-jhon.vercel.app",
+  origin: "https://yohanis-vehicle-management-system.vercel.app",
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
 };
