59ab59326996ce6dde9ded457f42bcbb9afd3055
Yohanis Tadese <127344303+yohanis-tadese@users.noreply.github.com>
2024-12-14 01:21:42 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://yohanistadese05:Rgw3GwsPEDDdATQX@cluster0.7cuyu.mongodb.net/");
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error.message);
-    process.exit(1);
+  if (!isConnected) {
+    try {
+      await mongoose.connect("mongodb+srv://yohanistadese05:Rgw3GwsPEDDdATQX@cluster0.7cuyu.mongodb.net/");
+      isConnected = true;
+    } catch (error) {
+      throw error;
+    }
   }
 };
 
-app.listen(5000, async () => {
-  await connectDB();
-  console.log(`Server is running on port 5000}`);
-});
+export default async (req, res) => {
+  try {
+    await connectDB();
+    app(req, res);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
