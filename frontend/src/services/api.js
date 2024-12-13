a47c8ea5bac7b2e45b8d87cbd00d61a225809410
Yohanis Tadese <127344303+yohanis-tadese@users.noreply.github.com>
2024-12-13 22:54:06 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://vehicle-management-system-neon.vercel.app/api/",
+  baseURL: "https://vehicle-management-system-neon.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
